f864ac1b45dd76dd07d5e753782ca1233a557796
Mayank <9084735+mayank99@users.noreply.github.com>
2021-06-08 17:44:06 -0400

    Add storysource addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-storysource",
   ],
   "webpackFinal": (config) => {
       const path = require('path');
